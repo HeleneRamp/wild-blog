72d72718a9bda39a38268866eb659cf5b04869d8
Hélène Rampelberg <h.rampelberg@yahoo.fr>
2024-10-01 14:33:22 +0200

    finished quest seven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Hélène 😸';
 
     }
---
+++ src/app/models/Message.ts
@@ -0,0 +1,7 @@
+
+export interface Message {
+    lastName: string;
+    firstName: string;
+    email: string;
+    content: string;
+}
