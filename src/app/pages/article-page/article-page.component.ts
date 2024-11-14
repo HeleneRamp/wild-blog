58fe2cf7d1e25f965ff80d3e0236925ce7bd8df0
Hélène Rampelberg <h.rampelberg@yahoo.fr>
2024-11-14 09:38:12 +0100

    service quest completed

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,9 +1,9 @@
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { map, Observable, switchMap } from 'rxjs';
 import { Article } from '../../models/Article';
 import { ActivatedRoute, ParamMap, RouterLink } from '@angular/router';
+import { ApiService } from '../../services/api.service';
 
 
 @Component({
@@ -14,9 +14,9 @@ import { ActivatedRoute, ParamMap, RouterLink } from '@angular/router';
   styleUrl: './article-page.component.scss',
 })
 export class ArticlePageComponent {
-  private apiUrl = 'http://localhost:3000/articles';
 
-  private http: HttpClient = inject(HttpClient)
+  private apiService: ApiService = inject(ApiService)
+
   private route: ActivatedRoute = inject(ActivatedRoute)
   private changeDetector: ChangeDetectorRef = inject(ChangeDetectorRef)
 
@@ -27,12 +27,9 @@ export class ArticlePageComponent {
     this.article$ = this.route.paramMap.pipe(switchMap((params: ParamMap) => {
         this.articleId = Number(params.get('id'));
         this.changeDetector.detectChanges();
-        return this.getArticleById(this.articleId)
+        return this.apiService.getArticleById(this.articleId)
       })
     )
     }
     
-    getArticleById(id: number): Observable<Article> {
-    return this.http.get<Article>(`${this.apiUrl}/${id}`)
-  }
 }
